ab62f77f7b4c27bbf1c9f09d204dd77d15704b4b
guymoyo <guymoyo@yahoo.fr>
2015-01-09 17:09:53 +0100

    list and search login

--- adcom/adbase.client/src/main/webapp/modules/login/controllers.js
+++ adcom/adbase.client/src/main/webapp/modules/login/controllers.js
@@ -1,9 +1,82 @@
-﻿'use strict';
+﻿(function () {
+    'use strict';
+    angular.module('AdBase').controller('loginController',loginController);
 
-angular.module('AdBase')
-.controller('loginController',['$scope','loginService',function ($scope,loginService) {
-		$scope.logins = function(){
-			return loginService.logins;
-		}; 
-    }]
-);
+    loginController.$inject = ['$scope', 'loginService'];
+
+    function loginController($scope,loginService){
+        var self = this ;
+
+        self.searchInput = {};
+        self.totalItems ;
+        self.itemPerPage=25 ;
+        self.currentPage = 1;
+        self.maxSize = 5 ;
+        self.logins = [];
+        self.searchEntity = {};
+        self.logins = [];
+        self.selectedLogin = {} ;
+        self.selectedIndex  ;
+        self.handleSearchRequestEvent = handleSearchRequestEvent;
+        self.handlePrintRequestEvent = handlePrintRequestEvent;
+        self.partner = {};
+
+        init();
+
+        function init(){
+
+            self.searchInput = {
+                entity:{},
+                fieldNames:[],
+                start:0,
+                max:self.itemPerPage
+            }
+
+            loginService.loadLogins().then(function(entitySearchResult) {
+                self.logins = entitySearchResult.resultList;
+                self.totalItems = entitySearchResult.count ;
+            });
+
+
+        }
+
+        function processSearchInput(searchEntity){
+            var fileName = [];
+
+            if(searchEntity.loginName){
+                fileName.push('loginName') ;
+                self.searchInput.entity.loginName = searchEntity.loginName ;
+            }
+
+            if(searchEntity.fullName){
+                fileName.push('fullName') ;
+                self.searchInput.entity.fullName = searchEntity.fullName ;
+            }
+
+            if(searchEntity.ouIdentif){
+                fileName.push('ouIdentif') ;
+                self.searchInput.entity.fullName = searchEntity.ouIdentif ;
+            }
+
+            self.searchInput.fieldNames = fileName ;
+            return self.searchInput ;
+        };
+
+        function  handleSearchRequestEvent(){
+
+            var searchInput =   processSearchInput(self.searchEntity);
+            loginService.findLogins(searchInput).then(function(data){
+                self.logins = data.resultList;
+            })
+
+        };
+
+
+        function handlePrintRequestEvent(){
+
+        }
+    };
+
+
+
+})();
