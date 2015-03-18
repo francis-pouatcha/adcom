207048d31c0575b80b42696edb130423592d5c93
dassiorleando <dassi_orleando@yahoo.fr>
2015-03-18 11:47:14 +0100

    edit of jpa AptAptmt

--- adcom/adAptmt.server/src/main/java/org/adorsys/adaptmt/jpa/AptmtStatus.java
+++ adcom/adAptmt.server/src/main/java/org/adorsys/adaptmt/jpa/AptmtStatus.java
@@ -2,12 +2,11 @@ package org.adorsys.adaptmt.jpa;
 
 import org.adorsys.javaext.description.Description;
 
-public enum AptmtStatus
-{
-   @Description("AptmtStatus_FORTHCOMMING_description")
-   FORTHCOMMING, @Description("AptmtStatus_ONGOING_description")
-   ONGOING, @Description("AptmtStatus_CANCEL_description")
-   CANCEL, @Description("AptmtStatus_CLOSED_description")
-   CLOSED, @Description("AptmtStatus_REPPORTED_description")
-   REPPORTED
+public enum AptmtStatus {
+	@Description("AptmtStatus_FORTHCOMMING_description")
+	FORTHCOMMING, @Description("AptmtStatus_ONGOING_description")
+	ONGOING, @Description("AptmtStatus_CANCEL_description")
+	CANCEL, @Description("AptmtStatus_CLOSED_description")
+	CLOSED, @Description("AptmtStatus_REPPORTED_description")
+	REPPORTED
 }
