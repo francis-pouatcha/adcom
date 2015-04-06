7030849ff549a8b166a5dfbaabe134fde2f41741
dassiorleando <dassi_orleando@yahoo.fr>
2015-04-06 08:47:53 +0100

    display of adaptmts

--- adcom/adaptmt.client/src/main/webapp/scripts/app.js
+++ adcom/adaptmt.client/src/main/webapp/scripts/app.js
@@ -1,74 +1,127 @@
 'use strict';
 
-angular.module('adaptmt',['ngRoute',
-                          'ngCookies',
-                          'ui.bootstrap',
-                          'SessionManager',
-                          'AuthInterceptor',
-                          'ngSanitize',
-                          'pascalprecht.translate',
-                          'NavBar',
-                           'ngResource',
-                           'datePicker',
-                           'ADUtils',
-                           'httpProgress'])
-  
-   .constant('APP_CONFIG',{
-	'appName':'Appointment',
-	'appVersion':'1.0.0-SNAPSHOT'
+angular
+		.module(
+				'adaptmt',
+				[ 'ngRoute', 'ngCookies', 'ui.bootstrap', 'SessionManager',
+						'AuthInterceptor', 'ngSanitize',
+						'pascalprecht.translate', 'NavBar', 'ngResource',
+						'datePicker', 'ADUtils', 'httpProgress' ])
 
-   })
-                          
-  .config(['$routeProvider', '$httpProvider','$translateProvider','$translatePartialLoaderProvider', function($routeProvider,$httpProvider,$translateProvider,$translatePartialLoaderProvider) {
-    $routeProvider
-      .when('/',{templateUrl:'views/aptaptmt/aptaptmts.html',controller:'aptAptmtsController'})
-      .when('/aptaptmt',{templateUrl:'views/aptaptmt/search.html',controller:'SearchAptAptmtController'})
-      .when('/aptaptmt/create',{templateUrl:'views/aptaptmt/create.html',controller:'aptAptmtCreateCtlr'})
-      .when('/aptaptmt/show/:id',{templateUrl:'views/aptaptmt/show.html',controller:'aptAptmtShowController'})
-      .when('/aptaptmt/update/:id',{templateUrl:'views/aptaptmt/update.html',controller:'aptAptmtUpdateController'})
-      .otherwise({
-        redirectTo: '/'
-      });
-    
-    $httpProvider.defaults.withCredentials = true;
-    $httpProvider.interceptors.push('authInterceptor');
-    
-    $translateProvider.useLoader('$translatePartialLoader', {
-        urlTemplate: '{part}/locale-{lang}.json'
-    });
-    
-  }])
-  
-  .controller('LandingPageController', function LandingPageController() {
-  })
-  
-  .controller('NavController', function NavController($scope, $location) {
-    $scope.matchesRoute = function(route) {
-        var path = $location.path();
-        return (path === ("/" + route) || path.indexOf("/" + route + "/") == 0);
-    };
-  })
-  
-  .run(['$rootScope', '$location','sessionManager','$translate','APP_CONFIG','$translatePartialLoader',
-      function ($rootScope, $location, sessionManager,$translate,APP_CONFIG,$translatePartialLoader) {
-    $rootScope.appName = APP_CONFIG.appName ;
-    $rootScope.appVersion = APP_CONFIG.appVersion ;
-    $translatePartialLoader.addPart('/adaptmt.client/i18n/main');
-    sessionManager.appMenuUrl("/adaptmt.client/menu.html");
-    $rootScope.sessionManager = sessionManager;
-    $rootScope.$on('$locationChangeStart', function (event, next, current) {
-    	var noSess = !sessionManager.hasValues(sessionManager.terminalSession(), sessionManager.userSession());
-    	if(noSess){
-			var sessParam = $location.search();
-			if(sessParam && sessionManager.hasValues(sessParam.trm,sessParam.usr)){
-				sessionManager.wsin(sessParam.trm,sessParam.usr,
-					function(data, status, headers, config){
-						sessionManager.language(headers('X-USER-LANG'),false);
-						$location.path('/');
-					}
-				);
-			}
-    	}
-    });
+		.constant('APP_CONFIG', {
+			'appName' : 'Appointment',
+			'appVersion' : '1.0.0-SNAPSHOT'
 
-}]);
+		})
+
+		.config(
+				[
+						'$routeProvider',
+						'$httpProvider',
+						'$translateProvider',
+						'$translatePartialLoaderProvider',
+						function($routeProvider, $httpProvider,
+								$translateProvider,
+								$translatePartialLoaderProvider) {
+							$routeProvider.when('/', {
+								templateUrl : 'views/aptaptmt/aptaptmts.html',
+								controller : 'aptAptmtsController',
+								controllerAs : 'aptAptmtsCtrl',
+								module : 'adaptmt'
+							}).when('/aptaptmt', {
+								templateUrl : 'views/aptaptmt/search.html',
+								controller : 'SearchAptAptmtController'
+							}).when('/aptaptmt/create', {
+								templateUrl : 'views/aptaptmt/create.html',
+								controller : 'aptAptmtCreateCtlr'
+							}).when('/aptaptmt/show/:id', {
+								templateUrl : 'views/aptaptmt/show.html',
+								controller : 'aptAptmtShowController'
+							}).when('/aptaptmt/update/:id', {
+								templateUrl : 'views/aptaptmt/update.html',
+								controller : 'aptAptmtUpdateController'
+							}).otherwise({
+								redirectTo : '/'
+							});
+
+							$httpProvider.defaults.withCredentials = true;
+							$httpProvider.interceptors.push('authInterceptor');
+
+							$translateProvider
+									.useLoader(
+											'$translatePartialLoader',
+											{
+												urlTemplate : '{part}/locale-{lang}.json'
+											});
+
+						} ])
+
+		.controller('LandingPageController', function LandingPageController() {
+		})
+
+		.controller(
+				'NavController',
+				function NavController($scope, $location) {
+					$scope.matchesRoute = function(route) {
+						var path = $location.path();
+						return (path === ("/" + route) || path.indexOf("/"
+								+ route + "/") == 0);
+					};
+				})
+
+		.run(
+				[
+						'$rootScope',
+						'$location',
+						'sessionManager',
+						'$translate',
+						'APP_CONFIG',
+						'$translatePartialLoader',
+						function($rootScope, $location, sessionManager,
+								$translate, APP_CONFIG, $translatePartialLoader) {
+							$rootScope.appName = APP_CONFIG.appName;
+							$rootScope.appVersion = APP_CONFIG.appVersion;
+							$translatePartialLoader
+									.addPart('/adaptmt.client/i18n/main');
+							sessionManager
+									.appMenuUrl("/adaptmt.client/menu.html");
+							$rootScope.sessionManager = sessionManager;
+							$rootScope
+									.$on(
+											'$locationChangeStart',
+											function(event, next, current) {
+												var noSess = !sessionManager
+														.hasValues(
+																sessionManager
+																		.terminalSession(),
+																sessionManager
+																		.userSession());
+												if (noSess) {
+													var sessParam = $location
+															.search();
+													if (sessParam
+															&& sessionManager
+																	.hasValues(
+																			sessParam.trm,
+																			sessParam.usr)) {
+														sessionManager
+																.wsin(
+																		sessParam.trm,
+																		sessParam.usr,
+																		function(
+																				data,
+																				status,
+																				headers,
+																				config) {
+																			sessionManager
+																					.language(
+																							headers('X-USER-LANG'),
+																							false);
+																			$location
+																					.path('/');
+																		});
+													}
+												}
+											});
+
+						} ]);
--- adcom/adaptmt.client/src/main/webapp/scripts/controllers/AptAptmtsController.js
+++ adcom/adaptmt.client/src/main/webapp/scripts/controllers/AptAptmtsController.js
@@ -5,33 +5,72 @@ angular.module("adaptmt")
 .controller('aptAptmtsController',['$scope','genericResource', '$translate', 'aptAptmtsService','$location','$rootScope',
                                 function($scope,genericResource, $translate, aptAptmtsService,$location,$rootScope){
 	
-   $scope.error = [];
-   $scope.aptAptmtsService=aptAptmtsService;
-   $scope.searchInput = {};
-   $scope.aptAptmts = {};
-   $scope.totalItems = 0;
-   $scope.aptAptmtsearchResults = {};
-   $scope.itemPerPage=25;
+   var self = this;
+	
+   self.error = [];
+   self.searchInput = {};
+   self.aptAptmts = [];
+   self.totalItems = 0;
+   self.aptAptmtsearchResults = {};
+   self.itemPerPage=25;
+   self.currentPage = 1;
+   self.maxSize = 5 ;
+   self.handleSearchRequestEvent = handleSearchRequestEvent;
+   self.handlePrintRequestEvent = handlePrintRequestEvent;
+   self.paginate = paginate;
+   self.logins = [];
    
-   $scope.init = function (){
+   function init(){
        
-        $scope.searchInput = {
+        self.searchInput = {
                 entity:{},
                 fieldNames:[],
                 start:0,
                 max:$scope.itemPerPage
             }
           
-       aptAptmtsService.loadAptAptmts($scope.searchInput).then(function(entitySearchResult) {
-                $scope.aptAptmts = entitySearchResult.resultList;
-                $scope.totalItems = entitySearchResult.count ;
+       aptAptmtsService.loadAptAptmts(self.searchInput).then(function(entitySearchResult) {
+    	   		self.aptAptmts = entitySearchResult.resultList;
+    	   		self.totalItems = entitySearchResult.count ;
             });
        
-       console.log($scope.totalItems);
+       console.log(self.totalItems);
        
-   }
+   };
    
-   $scope.init();
+   init();
+
+   function processSearchInput(){
+       var fileName = [];
+       if(self.searchInput.entity.loginName){
+           fileName.push('loginName') ;
+       }
+       if(self.searchInput.entity.fullName){
+           fileName.push('fullName') ;
+       }
+       if(self.searchInput.entity.ouIdentif){
+           fileName.push('ouIdentif') ;
+       }
+       self.searchInput.fieldNames = fileName ;
+       return self.searchInput ;
+   };
+
+   function  handleSearchRequestEvent(){
+        processSearchInput();
+       findByLike(self.searchInput);
+   };
+   
+   function paginate(){
+       self.searchInput.start = ((self.currentPage - 1)  * self.itemPerPage) ;
+       self.searchInput.max = self.itemPerPage ;
+       findByLike(self.searchInput);
+   };
+
+
+   function handlePrintRequestEvent(){
+
+   }
+
 
 }])
 
