5195e27c2f4f89d2a46cb31d224060577e361ba1
Boris Waguia <b.waguia@gmail.com>
2015-04-15 11:24:53 +0100

    Adding Abstract Pymnt Entities.

---
+++ adcom/adcshdwr.api/src/main/java/org/adorsys/adcshdwr/jpa/CdrPymntEvt.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.adorsys.adcshdwr.jpa;
+
+import javax.persistence.Entity;
+
+/**
+ * @author boriswaguia
+ *
+ */
+@Entity
+public abstract class CdrPymntEvt extends CdrAbstractPymnt{
+
+	/**
+	 * The serialVersionUID field.
+	 */
+	private static final long serialVersionUID = -7229869653492576128L;
+	
+}
---
+++ adcom/adcshdwr.api/src/main/java/org/adorsys/adcshdwr/jpa/CdrPymntItemEvt.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.adorsys.adcshdwr.jpa;
+
+import javax.persistence.Entity;
+
+/**
+ * @author boriswaguia
+ *
+ */
+
+@Entity
+public abstract class CdrPymntItemEvt extends CdrAbstractPymntItem{
+
+	/**
+	 * The serialVersionUID field.
+	 */
+	private static final long serialVersionUID = 7716930940613882353L;
+}
